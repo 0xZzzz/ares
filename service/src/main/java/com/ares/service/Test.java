248af9a8896393569166af02a38be84d1eee308b
fansheng.zq <fansheng.zq@alibaba-inc.com>
2020-03-26 19:04:25 +0800

    fix

--- service/src/main/java/com/ares/service/Test.java
+++ service/src/main/java/com/ares/service/Test.java
@@ -1,12 +1,5 @@
 package com.ares.service;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.google.common.collect.Maps;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Map;
-
 /**
  * @author fansheng
  * @date 2020/2/25
@@ -14,24 +7,8 @@ import java.util.Map;
 public class Test {
 
     public static void main(String[] args) {
-        String str = "[{\"type\": \"select\",\"value\": [\"3\", \"6\", \"12\", \"24\", \"36\"],\"key\": \"period\","
-            + "\"name\": \"分期数\"}]";
-        for (JSONObject json : JSONArray.parseArray(str, JSONObject.class)) {
-            Map<String, String> extra = Maps.newHashMap();
-            if (StringUtils.isNotBlank(json.getString("key"))) {
-                extra.put("key", json.getString("key"));
-            }
-            if (StringUtils.isNotBlank(json.getString("value"))) {
-                extra.put("value", json.getString("value"));
-            }
-            if (StringUtils.isNotBlank(json.getString("name"))) {
-                extra.put("name", json.getString("name"));
-            }
-            if (StringUtils.isNotBlank(json.getString("type"))) {
-                extra.put("type", json.getString("type"));
-            }
-            System.out.println(extra);
-        }
+        final short s = 30;
+        byte b = s;
     }
 
 }
---
+++ service/src/main/java/com/ares/service/algorithms/leetcode/easy/Demo1.java
@@ -0,0 +1,60 @@
+package com.ares.service.algorithms.leetcode.easy;
+
+/**
+ * @author fansheng
+ * @date 2020/3/26
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        // 棋盘
+        String[][] board = {
+            {".", ".", ".", ".", ".", ".", ".", "."},
+            {".", ".", ".", "p", ".", ".", ".", "."},
+            {".", ".", ".", "R", ".", ".", ".", "p"},
+            {".", ".", ".", ".", ".", ".", ".", "."},
+            {".", ".", ".", ".", ".", ".", ".", "."},
+            {".", ".", ".", "p", ".", ".", ".", "."},
+            {".", ".", ".", ".", ".", ".", ".", "."},
+            {".", ".", ".", ".", ".", ".", ".", "."}
+        };
+        algorithms(board);
+    }
+
+    private static void algorithms(String[][] board) {
+        // 首先找到 R 的坐标
+        int rx = -1, ry = -1;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ("R".equals(board[i][j])) {
+                    rx = j;
+                    ry = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(rx + "," + ry);
+        // 声明四个方向，例如 上：x + 1 y 不动，以此类推
+        int[][] d = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+        // 吃掉P的总数
+        int cnt = 0;
+        // 遍历四个方向
+        for (int i = 0; i < 4; i++) {
+            for (int step = 0; ; ++step) {
+                // x 轴的移动是纵坐标的变化
+                int tx = ry + d[i][1] * step;
+                // y 轴的移动是横坐标的变化
+                int ty = rx + d[i][0] * step;
+                if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8 || "B".equals(board[tx][ty])) {
+                    break;
+                }
+                if ("p".equals(board[tx][ty])) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+
+}

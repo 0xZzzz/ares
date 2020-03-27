ed1ea642e8f23b60c8fdf94b3d5dc64ca502d851
fansheng.zq <fansheng.zq@alibaba-inc.com>
2020-03-27 16:56:38 +0800

    fix

--- service/src/main/java/com/ares/service/algorithms/leetcode/easy/Demo3.java
+++ service/src/main/java/com/ares/service/algorithms/leetcode/easy/Demo3.java
@@ -1,81 +1,74 @@
 package com.ares.service.algorithms.leetcode.easy;
 
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
 /**
  * @author fansheng
- * @date 2020/3/26
+ * @date 2020/3/27
  */
 public class Demo3 {
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        ListNode l2 = new ListNode(4);
-        ListNode l3 = new ListNode(3);
-        l1.next = l2;
-        l2.next = l3;
-
-        ListNode l4 = new ListNode(5);
-        ListNode l5 = new ListNode(6);
-        ListNode l6 = new ListNode(4);
-        l4.next = l5;
-        l5.next = l6;
-
-        algorithms(l1, l4);
+        int[] deck = {1, 2, 3, 4, 4, 3, 2, 1};
+        algorithms1(deck);
+        algorithms2(deck);
     }
 
     /**
      * 时间复杂度 O(n) 空间复杂度 O(n)
      */
-    private static void algorithms(ListNode n1, ListNode n2) {
-        ListNode head = new ListNode(0);
-        ListNode l1 = n1, l2 = n2, curr = head;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int num1 = l1 == null ? 0 : l1.val;
-            int num2 = l2 == null ? 0 : l2.val;
-            int sum = num1 + num2 + carry;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (l1 != null) {
-                l1 = l1.next;
+    private static void algorithms1(int[] deck) {
+        Map<Integer, Integer> map = Maps.newHashMap();
+        for (int i : deck) {
+            map.merge(i, 1, (i1, i2) -> i1 + i2);
+        }
+        int x = -1;
+        for (Integer size : map.values()) {
+            if (size < 2) {
+                System.out.println(false);
+                return;
             }
-            if (l2 != null) {
-                l2 = l2.next;
+            if (x < 2) {
+                x = size;
+                continue;
+            }
+            if (size != x) {
+                System.out.println(false);
+                return;
             }
         }
-        if (carry > 0) {
-            curr.next = new ListNode(carry);
-        }
-
-        head.next.print();
+        System.out.println(true);
     }
 
     /**
-     * 链表节点
+     * 时间复杂度：O(n log c)，其中 n 是卡牌的个数，c 是数组 deck 中数的范围
+     * 空间复杂度：O(n)
      */
-    static class ListNode {
-        /**
-         * 值
-         */
-        int val;
-
-        /**
-         * 下个节点
-         */
-        ListNode next;
-
-        ListNode(int x) { val = x; }
-
-        void print() {
-            StringBuilder str = new StringBuilder(String.valueOf(val)).append(" ");
-            ListNode n = next;
-            while (n != null) {
-                str.append(String.valueOf(n.val)).append(" ");
-                n = n.next;
-            }
-            System.out.println(str.toString());
+    private static void algorithms2(int[] deck) {
+        int[] sum = new int[10000];
+        for (int i : deck) {
+            sum[i]++;
         }
+        int g = -1;
+        for (int i = 0; i < 10000; i++) {
+            if (sum[i] > 0) {
+                if (g == -1) {
+                    g = sum[i];
+                } else {
+                    g = gcd(g, sum[i]);
+                }
+            }
+        }
+        System.out.println(g >= 2);
+    }
 
+    /**
+     * 求最大公约数，时间复杂度 O(log c)
+     */
+    private static int gcd(int x, int y) {
+        return x == 0 ? y : gcd(y % x, x);
     }
 
 }
---
+++ service/src/main/java/com/ares/service/algorithms/leetcode/middle/Demo1.java
@@ -0,0 +1,81 @@
+package com.ares.service.algorithms.leetcode.middle;
+
+/**
+ * @author fansheng
+ * @date 2020/3/26
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l2 = new ListNode(4);
+        ListNode l3 = new ListNode(3);
+        l1.next = l2;
+        l2.next = l3;
+
+        ListNode l4 = new ListNode(5);
+        ListNode l5 = new ListNode(6);
+        ListNode l6 = new ListNode(4);
+        l4.next = l5;
+        l5.next = l6;
+
+        algorithms(l1, l4);
+    }
+
+    /**
+     * 时间复杂度 O(n) 空间复杂度 O(n)
+     */
+    private static void algorithms(ListNode n1, ListNode n2) {
+        ListNode head = new ListNode(0);
+        ListNode l1 = n1, l2 = n2, curr = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 == null ? 0 : l1.val;
+            int num2 = l2 == null ? 0 : l2.val;
+            int sum = num1 + num2 + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        head.next.print();
+    }
+
+    /**
+     * 链表节点
+     */
+    static class ListNode {
+        /**
+         * 值
+         */
+        int val;
+
+        /**
+         * 下个节点
+         */
+        ListNode next;
+
+        ListNode(int x) { val = x; }
+
+        void print() {
+            StringBuilder str = new StringBuilder(String.valueOf(val)).append(" ");
+            ListNode n = next;
+            while (n != null) {
+                str.append(String.valueOf(n.val)).append(" ");
+                n = n.next;
+            }
+            System.out.println(str.toString());
+        }
+
+    }
+
+}

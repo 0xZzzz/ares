a1850aec3b3b20119e19f94a930ab288dc681109
fansheng.zq <fansheng.zq@alibaba-inc.com>
2020-04-12 17:53:18 +0800

    fix

---
+++ service/src/main/java/com/ares/service/algorithms/leetcode/middle/Demo7.java
@@ -0,0 +1,37 @@
+package com.ares.service.algorithms.leetcode.middle;
+
+/**
+ * @author fansheng
+ * @date 2020/4/8
+ */
+public class Demo7 {
+
+    public static void main(String[] args) {
+        algorithms(33, 23, 30);
+    }
+
+    private static void algorithms(int m, int n, int k) {
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (calculateDigitSum(i) + calculateDigitSum(j) <= k) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    /**
+     * 计算数字的数位之和
+     */
+    private static int calculateDigitSum(int num) {
+        int res = 0;
+        do {
+            res += num % 10;
+            num /= 10;
+        } while (num > 10);
+        return res;
+    }
+
+}

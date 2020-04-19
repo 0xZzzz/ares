a5ac3a0e59c0f28a650eb2e0e3c07e22aec117cb
fansheng.zq <fansheng.zq@alibaba-inc.com>
2020-04-19 20:31:51 +0800

    fix

--- service/src/main/java/com/ares/service/algorithms/leetcode/middle/ZigZagConversion.java
+++ service/src/main/java/com/ares/service/algorithms/leetcode/middle/ZigZagConversion.java
@@ -5,45 +5,35 @@ import java.util.List;
 
 /**
  * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
- *
- * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
- * L   C   I   R
- * E T O E S I I G
- * E   D   H   N
- *
+ * <p>
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下： L   C   I   R E T O E S I I G E   D   H   N
+ * <p>
  * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
- *
+ * <p>
  * 请你实现这个将字符串进行指定行数变换的函数：
- *
+ * <p>
  * string convert(string s, int numRows);
- *
- * 示例 1:
- * 输入: s = "LEETCODEISHIRING", numRows = 3
- * 输出: "LCIRETOESIIGEDHN"
- *
- * 示例 2:
- * 输入: s = "LEETCODEISHIRING", numRows = 4
- * 输出: "LDREOEIIECIHNTSG"
- * 解释:
- *
- * L     D     R
- * E   O E   I I
- * E C   I H   N
- * T     S     G
+ * <p>
+ * 示例 1: 输入: s = "LEETCODEISHIRING", numRows = 3 输出: "LCIRETOESIIGEDHN"
+ * <p>
+ * 示例 2: 输入: s = "LEETCODEISHIRING", numRows = 4 输出: "LDREOEIIECIHNTSG" 解释:
+ * <p>
+ * L     D     R E   O E   I I E C   I H   N T     S     G
  *
  * @author fansheng
  * @date 2020/4/12
  */
-public class Demo8 {
+public class ZigZagConversion {
 
     public static void main(String[] args) {
-        convert("LEETCODEISHIRING", 3);
+        convert1("LEETCODEISHIRING", 3);
+        convert2("LEETCODEISHIRING", 3);
     }
 
     /**
      * 时间复杂度：O(n) 空间复杂度：O(n)
      */
-    private static void convert(String s, int numRows) {
+    private static void convert1(String s, int numRows) {
         if (numRows == 1) {
             return;
         }
@@ -70,6 +60,29 @@ public class Demo8 {
         StringBuilder ret = new StringBuilder();
         for (StringBuilder row : rows) {
             ret.append(row);
+        }
+        System.out.println(ret.toString());
+    }
+
+    /**
+     * 时间复杂度：O(n) 空间复杂度：O(n)
+     * 这个没懂，需要研究下
+     */
+    private static void convert2(String s, int numRows) {
+        if (numRows == 1) {
+            return;
+        }
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                    ret.append(s.charAt(j + cycleLen - i));
+                }
+            }
         }
         System.out.println(ret.toString());
     }

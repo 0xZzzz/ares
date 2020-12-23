a0166e8e243e10a9a6d7505b0793b290684a6245
fansheng <fansheng.zq@alibaba-inc.com>
2020-12-23 11:42:16 +0800

    调整格式

--- service/src/main/java/com/ares/service/algorithms/leetcode/middle/LongestPalindrome.java
+++ service/src/main/java/com/ares/service/algorithms/leetcode/middle/LongestPalindrome.java
@@ -1,7 +1,10 @@
 package com.ares.service.algorithms.leetcode.middle;
 
 /**
- * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/
+ * 5. 最长回文子串
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  *
  * 示例 1：
  * 输入: "babad"
@@ -15,7 +18,7 @@ package com.ares.service.algorithms.leetcode.middle;
  * @author  0xZzzz
  * @date 2020/4/7
  */
-public class Demo5 {
+public class LongestPalindrome {
 
     public static void main(String[] args) {
         algorithms("aa");

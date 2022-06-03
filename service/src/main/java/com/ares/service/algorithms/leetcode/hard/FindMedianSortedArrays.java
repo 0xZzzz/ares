c4d6069ad60ead1c4d7b7c67b20f0642e14000df
fansheng <fansheng.zq@alibaba-inc.com>
2022-06-03 23:20:42 +0800

    fix

--- service/src/main/java/com/ares/service/algorithms/leetcode/hard/FindMedianSortedArrays.java
+++ service/src/main/java/com/ares/service/algorithms/leetcode/hard/FindMedianSortedArrays.java
@@ -3,7 +3,7 @@ package com.ares.service.algorithms.leetcode.hard;
 /**
  * 4. 寻找两个正序数组的中位数
  * https://leetcode.cn/problems/median-of-two-sorted-arrays/
- *
+ * <p>
  * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数 。
  * <p>
  * 算法的时间复杂度应该为 O(log (m+n)) 。
@@ -33,38 +33,35 @@ package com.ares.service.algorithms.leetcode.hard;
 public class FindMedianSortedArrays {
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 6, 7, 8, 9};
+        int[] nums1 = new int[]{1, 2};
         int[] nums2 = new int[]{3, 4};
-        algorithms(nums2, nums1);
+        System.out.println(algorithms(nums1, nums2));
     }
 
-    private static void algorithms(int[] nums1, int[] nums2) {
+    private static double algorithms(int[] nums1, int[] nums2) {
         int totalLength = nums1.length + nums2.length;
         int mid = totalLength / 2;
         int p1 = 0;
         int p2 = 0;
-        int s = 0;
-        int e;
+        int start = 0;
+        int end;
+        int index = 0;
         while (true) {
-            int m = p1 == nums1.length - 1 ? Integer.MAX_VALUE : nums1[p1];
-            int n = p2 == nums2.length - 1 ? Integer.MAX_VALUE : nums2[p2];
-            if (p1 + p2 == mid) {
-                s = Math.min(m, n);
-            } else if (p1 + p2 == mid + 1) {
-                e = Math.min(m, n);
+            int m = p1 > nums1.length - 1 ? Integer.MAX_VALUE : nums1[p1];
+            int n = p2 > nums2.length - 1 ? Integer.MAX_VALUE : nums2[p2];
+            if (index == mid - 1) {
+                start = Math.min(m, n);
+            } else if (index == mid) {
+                end = Math.min(m, n);
                 break;
             }
-            if (m <= n || p2 == nums2.length - 1) {
+            if (m < n && p1 <= nums1.length - 1 || p2 >= nums2.length) {
                 p1++;
             } else {
                 p2++;
             }
+            index++;
         }
-        if (totalLength % 2 != 0) {
-            System.out.println(s);
-        } else {
-            System.out.println((s + e) / 2.0);
-        }
+        return totalLength % 2 != 0 ? end : ((start + end) / 2.0);
     }
-
 }
--- service/src/main/java/com/ares/service/algorithms/leetcode/practice/Practice.java
+++ service/src/main/java/com/ares/service/algorithms/leetcode/practice/Practice.java
@@ -7,19 +7,20 @@ package com.ares.service.algorithms.leetcode.practice;
 public class Practice {
 
     public static void main(String[] args) {
-        String input = "pwwkew";
-        String result = "";
-        StringBuilder current = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            String s = String.valueOf(c);
-            if (current.indexOf(s) < 0) {
-                current.append(s);
-            } else if (current.length() > result.length()) {
-                result = current.toString();
-                current = new StringBuilder(s);
-            }
+        System.out.println(isPalindrome(-121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
         }
-        System.out.println(result);
+        int t = x;
+        int r = 0;
+        while (t > 0) {
+            r = r * 10 + t % 10 ;
+            t /= 10;
+        }
+        return r == x;
     }
 
 }

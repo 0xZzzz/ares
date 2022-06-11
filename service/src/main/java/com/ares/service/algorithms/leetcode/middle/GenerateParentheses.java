e923df34d35856c1c340bcd08adf4b9c33f79b5f
fansheng <fansheng.zq@alibaba-inc.com>
2022-06-11 16:35:02 +0800

    题目初始化

---
+++ service/src/main/java/com/ares/service/algorithms/leetcode/middle/GenerateParentheses.java
@@ -0,0 +1,29 @@
+package com.ares.service.algorithms.leetcode.middle;
+
+/**
+ * 22. 括号生成
+ * https://leetcode.cn/problems/generate-parentheses/
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且有效的括号组合。
+ *
+ * 示例 1：
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ *
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：["()"]
+ *
+ * 提示：
+ * 1 <= n <= 8
+ *
+ * @author fansheng
+ * @date 2022/6/11
+ */
+public class GenerateParentheses {
+
+    public static void main(String[] args) {
+
+    }
+
+}

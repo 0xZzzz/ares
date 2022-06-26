934b3cf8792c9727e3bba05478b39745651fd864
fansheng <fansheng.zq@alibaba-inc.com>
2022-06-26 15:52:59 +0800

    middle 17

--- service/src/main/java/com/ares/service/algorithms/leetcode/middle/LetterCombinationsOfAPhoneNumber.java
+++ service/src/main/java/com/ares/service/algorithms/leetcode/middle/LetterCombinationsOfAPhoneNumber.java
@@ -1,5 +1,10 @@
 package com.ares.service.algorithms.leetcode.middle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 17. 电话号码的字母组合
  * https://leetcode.cn/problems/letter-combinations-of-a-phone-number/
@@ -29,7 +34,42 @@ package com.ares.service.algorithms.leetcode.middle;
 public class LetterCombinationsOfAPhoneNumber {
 
     public static void main(String[] args) {
+        self("234");
+    }
 
+    private static List<String> self(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuilder());
+        return combinations;
+    }
+
+    private static void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits,
+                                  int index, StringBuilder combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
     }
 
 }
